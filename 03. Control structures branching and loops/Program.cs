818f5f3caf222581edb7c45a156a963a2de9f2a1
TIERE Saïd Stéphane <58536528+TIERESAID@users.noreply.github.com>
2022-03-25 03:10:32 +0100

    Add files via upload

---
+++ 03. Control structures branching and loops/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace _03._Control_structures_branching_and_loops
+{
+    class Program
+    {
+        static  void Main(string[] args)
+        {
+
+            int numberOfPlayer ;
+
+            Console.WriteLine("How many player:");
+            numberOfPlayer = Convert.ToInt32(Console.ReadLine());
+            string[] playersName = new string[numberOfPlayer];
+
+            for(int i = 0 ; i<numberOfPlayer ; i++){
+                Console.WriteLine($"Enter the name of player {i+1}");
+                playersName[i] = Console.ReadLine();
+            }
+
+            Random rand = new Random();
+            int RandomNumberGuessedbyComputer = rand.Next(12,123);
+            Console.WriteLine(RandomNumberGuessedbyComputer);
+
+            int j = 0 ;
+            int x = 0 ;
+            do{
+                if( j==numberOfPlayer )
+                    j = 0;
+                
+                do
+                {
+                    Console.WriteLine($"Player[{j+1}]   ({playersName[j]}) : Enter a between [1-4]");
+                    x = Convert.ToInt32(Console.ReadLine());
+                }while(x<1 || x >4);
+
+                // Soustraction with x
+                RandomNumberGuessedbyComputer = RandomNumberGuessedbyComputer - x ;
+                if(RandomNumberGuessedbyComputer < 0){
+                    Console.WriteLine($"{playersName[j]} WIN THE GAME!" );
+                    Console.WriteLine($"{playersName[j]} DO YOU WANT TO TAKE REVENGE? (YES OR NO)" );
+                    string yesORno = Console.ReadLine();
+                    if(yesORno == "YES" || yesORno == "yes"){
+                        RandomNumberGuessedbyComputer = rand.Next(12,123);
+                        j = -1;
+                    }
+                    else if(yesORno == "N0" || yesORno == "no"){
+                        RandomNumberGuessedbyComputer = 0;
+                        Console.WriteLine("END OF THE GAME");
+                    }
+                }
+                j = j + 1 ;
+            }while(RandomNumberGuessedbyComputer > 0);
+            
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 03. Control structures branching and loops/README.md
@@ -0,0 +1,35 @@
+// Write a game that two players can play.
+            // At startup, players are prompted to enter their nicknames.
+            // Nicknames are kept until the end of the game.
+            // The program guesses a random number gameNumber from 12 to 120 and reports this number to the players.
+            // Players move in turn (the game reports the current player's move)
+            // The player whose turn is specified enters the userTry number, which can take the values ​​1, 2, 3 or 4,
+            // entered number is subtracted from gameNumber
+            // The new value of gameNumber is shown to the players on the screen.
+            // The player who turns gameNumber to zero wins.
+            // The game congratulates the winner, offering to play revenge
+            //
+            // * Bonus:
+            // Think about the possibility of implementing different levels of complexity.
+            // Difficulty levels can be configurable, at the beginning of the game,
+            // userTry value, changing gameNumber range, or specifying more players (3, 4, 5...)
+
+            // *** Complex bonus
+            // Think about the possibility of implementing a single-player game
+            // i.e. the player must play with the computer
+
+
+            // Demo
+            // Number: 12,
+            // Move User1: 3
+            //
+            // Number: 9
+            // User2 turn: 4
+            //
+            // Number: 5
+            // Move User1: 2
+            //
+            // Number: 3
+            // User2 turn: 3
+            //
+            // User2 wins!
